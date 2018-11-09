a51674a5beadae4206bac69d5784f638c8d22f0e
FuyuYasum <makertuwem@outlook.com>
2018-11-09 12:09:54 +0800

    +parentheses matching (recursive)

--- myStack.h
+++ myStack.h
@@ -5,13 +5,13 @@
 template <typename T> class Stack: public Vector<T> {
 public:
     void push(T const& e) {
-        insert(size(), e);
+        this->insert(this->size(), e);
     }
     T pop() {
-        return remove(size() - 1);
+        return remove(this->size() - 1);
     }
     T& top() {
-        return (*this)[size() - 1];
+        return (*this)[this->size() - 1];
     }
 };
 
@@ -34,5 +34,41 @@ void convertIter(Stack<char>& S, __int64 n, int base) {
         S.push(digit[remainder]);
         n /= base;
     }
+}
+
+// 删除表达式exp[lo, hi]不含括号的最长前缀、后缀
+void trim(const char exp[], int& lo, int& hi) {
+    while((lo <= hi) && (exp[lo] != '(') && (exp[lo] != ')'))
+        ++lo;
+    while((lo <= hi) && (exp[hi] != '(') && (exp[hi] != ')'))
+        --hi;
+}
+
+// 切分表达式exp[lo, hi], 使exp 匹配仅当它们匹配
+int divide(const char exp[], int lo, int hi) {
+    int mi = lo;
+    int crc = 1;
+    while((0 < crc) && (++mi < hi)) {
+        if(exp[mi] == ')')
+            --crc;
+        if(exp[mi] == '(')
+            ++crc;
+    }
+    return mi;
+}
+
+// 检查表达式exp[lo, hi] 是否括号匹配（递归版）
+bool parenIter(const char exp[], int lo, int hi) {
+    trim(exp, lo, hi);
+    if(lo > hi)
+        return true;
+    if(exp[lo] != '(')
+        return false;
+    if(exp[hi] != ')')
+        return false;
+    int mi = divide(exp, lo, hi);
+    if(mi > hi)
+        return false;
+    return paren(exp, lo + 1, mi -1) && paren(exp, mi + 1, hi);
 }
 #endif //DSA_CPP_DENG_MYSTACK_H
