7e8fc7381357c50d2b819566ef06e4cf85d35c21
ubuntuNatsu <makertuwem@outlook.com>
2019-05-05 14:41:05 +0800

    [optimize]open all

--- BTNode.h
+++ BTNode.h
@@ -7,14 +7,13 @@
  * B-树节点模板类
  */
 template <typename T> struct BTNode {
-protected:
+public:
     BTNode* parent;
     // 各相邻孩子之间的关键码组织为一个向量
     Vector<T> key;
     // 同一节点的所有孩子组织为一个向量
     Vector<BTNode*> child;
 
-public:
     /**
      * 构造函数
      * 注意：BTNode 只能作为根节点创建，而且初始时只有０个关键码，１个空的孩子指针
