cd92481d0f1ba141fc621488c29b36220da69cd0
ubuntuNatsu <makertuwem@outlook.com>
2019-05-07 22:40:36 +0800

    [update]implement the insert, solveDoubleRed and remove method for Class RedBlack

--- myRedBlackTree.h
+++ myRedBlackTree.h
@@ -32,4 +32,83 @@ int RedBlack<T>::updateHeight(BinNode<T> *x) {
     return IsRed(x) ? x->height : x->height++;
 }
 
+template<typename T>
+BinNode<T> *RedBlack<T>::insert(const T &e) {
+    BinNodePosi(T)& x = search(e);
+    if(x)
+        return x;
+    x = new BinNode<T>(e, this->_hot, nullptr, nullptr, -1);  // 留意 _hot，留意 -1 的使用
+    solveDoubleRed(x);
+    return x;
+}
+
+template<typename T>
+void RedBlack<T>::solveDoubleRed(BinNode<T> *x) {
+    if(IsRoot(*x)) {  // 递归基，到达根节点则染成黑色，高度上升一层
+        this->_root->color = RB_BLACK;
+        this->_root->height++;
+        return;
+    }
+    BinNodePosi(T) p = x->parent;
+    if(IsBlack(p))
+        return;
+    BinNodePosi(T) g = p->parent;
+    BinNodePosi(T) u = uncle(*x);
+    if(IsBlack(u)) {
+        if(IsLChild(*x) == IsLChild(*p)) {
+            p->color = RB_BLACK;
+        } else {  // 父子不同侧时
+            x->color = RB_BLACK;
+        }
+        g->color = RB_RED;
+        // 以上虽保证总共两次染色,但因增加了判断而得不偿失
+        // 可改进为：在旋转后将根置黑、孩子置红,虽需三次染色但效率更高
+        BinNodePosi(T) gg = g->parent;
+        BinNodePosi(T) r = FromParentTo(*g) = rotateAt(x);  // 旋转，然后让曾祖父连接上旋转后的子树的根节点
+        r->parent = gg;
+    } else {  // 叔叔为红时
+        // 无论父子同不同侧
+        p->color = RB_BLACK;
+        p->height++;
+        u->color = RB_BLACK;
+        u->height++;
+        if(!IsRoot(*g))
+            g->color = RB_RED;
+        solveDoubleRed(g);
+    }
+
+}
+
+template<typename T>
+bool RedBlack<T>::remove(const T &e) {
+    BinNodePosi(T)& x = search(e);
+    if(!x)
+        return false;
+    BinNodePosi(T) r = removeAtStatic(x, this->_hot);  // r 为取代 x 者
+    if(--this->_size <= 0)
+        return true;
+    // assert: _hot某一孩子刚被切除,且被r所指节点(可能是NULL)接替。以下检查是否失衡,幵做必要调整
+    if(!this->_hot) {  // 若被删除的节点为根节点，则将其置黑，更新其高度
+        this->_root->color = RB_BLACK;
+        updateHeight(this->_root);
+        return true;
+    }
+    // assert: 以下,原x(现r)必非根,_hot必非空
+    if(BlackHeightUpdated(*(this->_hot)))  // 若所有祖先的黑高度依然平衡，则无需调整
+        return true;
+    if(IsRed(r)) {  // 若 r 为红，只需将其置黑，并更新其高度
+        r->color = RB_BLACK;
+        r->height++;
+        return true;
+    }
+    // assert: 以下,原x(现r)均为黑色
+    solveDoubleBlack(r);
+    return true;
+}
+
+template<typename T>
+void RedBlack<T>::solveDoubleBlack(BinNode<T> *x) {
+    // todo
+}
+
 #endif //DSA_CPP_DENG_MYREDBLACKTREE_H
